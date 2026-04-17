b4ab4f56ecfcdca5945de8d7da8174ba04dd5a20
inseongsong <inseongsong0512@gmail.com>
2026-04-17 14:36:37 +0900

    favicon

--- lib/features/common/utils/constants.dart
+++ lib/features/common/utils/constants.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+class FeatureCardData {
+  final String title;
+  final String subtitle;
+  final String desc;
+  final String route;
+
+  const FeatureCardData({required this.title, required this.subtitle, required this.desc, required this.route});
+}
+
 class HeroSlide {
   final String subtitle;
   final String title;
@@ -41,23 +50,11 @@ class AppConstants {
   ];
 
   // Feature Cards 데이터
-  static const List<Map<String, String>> featureCards = [
-    {'icon': 'assets/icons/icon_workshop.svg', 'title': 'WORKSHOP', 'subtitle': '공방 소개', 'desc': '공방에 대한 소개와 문화, 그리고 추구하는 목표와 방향을 이야기합니다.', 'route': '/about'},
-    {'icon': 'assets/icons/icon_works.svg', 'title': 'WORKS', 'subtitle': '작품과 작업들', 'desc': '오로라공방 구성원분들이 제작한 작품들과 작업들을 소개합니다.', 'route': '/works'},
-    {
-      'icon': 'assets/icons/icon_class.svg',
-      'title': 'CLASS',
-      'subtitle': '다양한 목공 수업',
-      'desc': '가구 디자인 및 목공 기술을 배울 수 있는 전문적인 수업에 참여하실 수 있습니다.',
-      'route': '/class',
-    },
-    {
-      'icon': 'assets/icons/icon_membership.svg',
-      'title': 'MEMBERSHIP',
-      'subtitle': '공방 자유이용',
-      'desc': '개인 작품활동, 지속적인 취미목공 등을 위해 공방을 자유롭게 이용할 수 있습니다.',
-      'route': '/membership',
-    },
+  static const List<FeatureCardData> featureCards = [
+    FeatureCardData(title: 'WORKSHOP', subtitle: '공방 소개', desc: '공방에 대한 소개와 문화, 그리고 추구하는 목표와 방향을 이야기합니다.', route: '/about'),
+    FeatureCardData(title: 'WORKS', subtitle: '작품과 작업들', desc: '오로라공방 구성원분들이 제작한 작품들과 작업들을 소개합니다.', route: '/works'),
+    FeatureCardData(title: 'CLASS', subtitle: '다양한 목공 수업', desc: '가구 디자인 및 목공 기술을 배울 수 있는 전문적인 수업에 참여하실 수 있습니다.', route: '/class'),
+    FeatureCardData(title: 'MEMBERSHIP', subtitle: '공방 자유이용', desc: '개인 작품활동, 지속적인 취미목공 등을 위해 공방을 자유롭게 이용할 수 있습니다.', route: '/membership'),
   ];
 
   // FID 섹션 데이터
